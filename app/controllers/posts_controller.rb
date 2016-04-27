93550cc08e886ab7e2a22945df10ea8c028a438e
tgaff <tgaff@alumni.nd.edu>
2016-04-27 10:30:21 -0700

    added 'new' and 'create' methods and 'new' view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,25 @@ class PostsController < ApplicationController
     puts "PostsController#index found #{@posts.length} posts!"
     render :index
   end
+
+  def new
+    # assign an empty and blank Post to use in our view
+    @post = Post.new
+    render :new # this is optional
+  end
+
+
+  def create
+    post_params = params.require(:post).permit(:title, :body)
+
+    post = Post.new(post_params)
+
+    if post.save
+      redirect_to posts_path # go back to /posts index page
+    else
+      render :new # in case of error, re-render the '/posts/new' page
+    end
+  end
+
+
 end
