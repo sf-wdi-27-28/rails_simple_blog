76f2c312c38639470d77a87f154cecabfc532e21
tgaff <tgaff@alumni.nd.edu>
2016-04-27 10:10:23 -0700

    added new routes for writing blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  # /posts/new
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+
+  # /posts
+  get 'posts', to: 'posts#index'
+  post 'posts', to: 'posts#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
